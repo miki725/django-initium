17ac9b9473d1c0d50255962c5e4729fddb6f3c77
Miroslav Shubernetskiy <miroslav@miki725.com>
2013-12-07 22:16:21 -0500

    fixed typo

--- init/init-jquery.sh
+++ init/init-jquery.sh
@@ -3,11 +3,11 @@
 LIB_PATH=../{{ project_name }}/{{ project_name }}/static/lib/jquery
 NAME=jquery.min.js
 
-echo "Prepping project for html5shiv"
-echo "------------------------------"
+echo "Prepping project for jquery"
+echo "---------------------------"
 rm -rf $LIB_PATH
 mkdir -p $LIB_PATH
 
-echo "Downloading html5shiv"
-echo "---------------------"
+echo "Downloading jquery"
+echo "------------------"
 wget -O $LIB_PATH/$NAME http://code.jquery.com/jquery-latest.min.js --quiet
